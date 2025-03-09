be37fa6a95ee02c2762584fa849e6cce9e06eab1
Jacó Wally <jacowally8@gmail.com>
2025-03-09 18:14:31 -0300

    Alguns ajustes no css do Topo

--- resources/js/components/admin/Topo.vue
+++ resources/js/components/admin/Topo.vue
@@ -21,51 +21,53 @@
         </label>
       </div>
 
-      <!-- usuarios -->
-      <div class="area-item some-mobile" @click="showDropUsuarios">
-        <div class="usuario-icone">
-          <img src="../../../img/usuarios.png" alt="Usuários" />
+      <div class="topo-conteudo">
+        <!-- usuarios -->
+        <div class="area-item some-mobile" @click="showDropUsuarios">
+          <div class="usuario-icone">
+            <img src="../../../img/usuarios.png" alt="Usuários" />
+          </div>
+
+          <div class="usuario-texto">
+            <h2>Usuários</h2>
+            <img
+              src="../../../img/seta_baixo.png"
+              :class="['seta', { rotate: mudarSeta }]"
+            />
+          </div>
+
+          <div v-show="DropContUsuarios" class="drop-topo">
+            <ul>
+              <li>
+                <router-link to="/usuarios/cadastrar">
+                  <a>Cadastrar</a>
+                </router-link>
+              </li>
+              <li>
+                <a href="#">Listar</a>
+              </li>
+            </ul>
+          </div>
         </div>
 
-        <div class="usuario-texto">
-          <h2>Usuários</h2>
-          <img
-            src="../../../img/seta_baixo.png"
-            :class="['seta', { rotate: mudarSeta }]"
-          />
+        <!-- configurações -->
+        <div class="area-item some-mobile">
+          <div class="conf-icone">
+            <img src="../../../img/config.png" alt="Configurações" />
+          </div>
+          <div class="conf-texto">
+            <h2>Configurações</h2>
+          </div>
         </div>
 
-        <div v-show="DropContUsuarios" class="drop-topo">
-          <ul>
-            <li>
-              <router-link to="/usuarios/cadastrar">
-                <a>Cadastrar</a>
-              </router-link>
-            </li>
-            <li>
-              <a href="#">Listar</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <!-- configurações -->
-      <div class="area-item some-mobile">
-        <div class="conf-icone">
-          <img src="../../../img/config.png" alt="Configurações" />
-        </div>
-        <div class="conf-texto">
-          <h2>Configurações</h2>
-        </div>
-      </div>
-
-      <!-- sair -->
-      <div class="area-sair some-mobile">
-        <div class="sair-texto">
-          <h2>Sair</h2>
-        </div>
-        <div class="sair-icone">
-          <img src="../../../img/sair.png" alt="Configurações" />
+        <!-- sair -->
+        <div class="area-sair some-mobile">
+          <div class="sair-texto">
+            <h2>Sair</h2>
+          </div>
+          <div class="sair-icone">
+            <img src="../../../img/sair.png" alt="Configurações" />
+          </div>
         </div>
       </div>
 
@@ -88,7 +90,6 @@
             <div :class="['drop-topo', { apareceDrop: mover }]">
               <ul>
                 <li>
-                  <!-- <li @click="navegarPara('CadastrarUsuarios')"> -->
                   <router-link to="/usuarios/cadastrar">
                     <a @click="showDropConfigs">Cadastrar</a>
                   </router-link>
@@ -105,6 +106,7 @@
           </div>
         </div>
       </div>
+
     </nav>
   </header>
 
@@ -143,8 +145,10 @@ export default {
       this.rotate = !this.rotate;
     },
     showDropUsuarios2() {
-      this.mudarSeta2 = !this.mudarSeta2; // Muda o dropdown
-      this.mover = !this.mover; // Altera o estado da seta
+      // Altera o estado da seta
+      this.mudarSeta2 = !this.mudarSeta2; 
+      // Muda o dropdown
+      this.mover = !this.mover; 
     },
     navegarPara(p) {
       this.$emit("navegar", p);
